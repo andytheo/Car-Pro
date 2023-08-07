29ccc43f0a8b96df8d2a4fa97ee2fffec2a2d17f
andytheo <igbeinkutuandrew@yahoo.com>
2023-08-07 18:41:06 -0400

    Initial commit - Migrating from Gitlab to Github

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,20 @@ public class PlayerController : MonoBehaviour
         Vector3 horizontalMovement = transform.right * horizontalDirection * speed
             * Time.fixedDeltaTime * horizontalDirectionSpeed;
 
+        // Accelerating and decelerrating
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed += 2f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed -= .5f;
+        }
+
         rb.MovePosition(rb.position + forwardMovement + horizontalMovement);
+
+        // Keep car within bounds of the road
+        //ClampCarPosition();
     }
 
     // Update is called once per frame
@@ -30,7 +43,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "coin")
+        if (other.CompareTag("coin"))
         {
             gameManager.IncreaseScore();
 
@@ -38,5 +51,32 @@ public class PlayerController : MonoBehaviour
             Destroy(g, 2);
             Destroy(other.gameObject);
         }
+    }
+
+    private void ClampCarPosition()
+    {
+        Vector3 carPosition = transform.position;
+
+        // Get bounds of the road collider
+        Collider roadCollider = GameObject.FindGameObjectWithTag("Road").GetComponent<Collider>();
+        Bounds roadBounds = roadCollider.bounds;
+
+        // Clamp car position within the road bounds
+        float xClamp = Mathf.Clamp(carPosition.x, roadBounds.min.x, roadBounds.max.x);
+
+        // Check if the car is near the road boundary
+        bool nearBoundaryX = carPosition.x < roadBounds.min.x + 1f || carPosition.x > roadBounds.max.x - 1f;
+
+        // Apply a sliding force if the car is near a boundary
+        if (nearBoundaryX)
+        {
+            float forceX = (xClamp - carPosition.x) * speed;
+            rb.AddForce(Vector3.right * forceX);
+        }
+
+        carPosition = new Vector3(xClamp, carPosition.y, carPosition.z);
+
+        // Update car's position
+        transform.position = carPosition;
     }
 }
