65ad22f529e7145dbcf2299f3064f42791488899
andytheo <igbeinkutuandrew@yahoo.com>
2023-08-17 22:10:09 -0400

    Added UI functionality such as pause game, resume and quit - Added a main menu scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,16 +29,16 @@ public class PlayerController : MonoBehaviour
             speed -= .5f;
         }
 
-        rb.MovePosition(rb.position + forwardMovement + horizontalMovement);
-
-        // Keep car within bounds of the road
-        //ClampCarPosition();
+        if (!PauseMenu.isPaused)
+        {
+            rb.MovePosition(rb.position + forwardMovement + horizontalMovement);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalDirection = Input.GetAxis("Horizontal");
+       horizontalDirection = Input.GetAxis("Horizontal");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -53,30 +53,4 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void ClampCarPosition()
-    {
-        Vector3 carPosition = transform.position;
-
-        // Get bounds of the road collider
-        Collider roadCollider = GameObject.FindGameObjectWithTag("Road").GetComponent<Collider>();
-        Bounds roadBounds = roadCollider.bounds;
-
-        // Clamp car position within the road bounds
-        float xClamp = Mathf.Clamp(carPosition.x, roadBounds.min.x, roadBounds.max.x);
-
-        // Check if the car is near the road boundary
-        bool nearBoundaryX = carPosition.x < roadBounds.min.x + 1f || carPosition.x > roadBounds.max.x - 1f;
-
-        // Apply a sliding force if the car is near a boundary
-        if (nearBoundaryX)
-        {
-            float forceX = (xClamp - carPosition.x) * speed;
-            rb.AddForce(Vector3.right * forceX);
-        }
-
-        carPosition = new Vector3(xClamp, carPosition.y, carPosition.z);
-
-        // Update car's position
-        transform.position = carPosition;
-    }
 }
